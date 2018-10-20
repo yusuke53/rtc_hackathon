b2353b1ad8a6cfe9c8eb0b72f7a3f72fd26c04da
Yamaki <yusuke.yamaki@rakuten.com>
2018-10-20 20:40:04 +0900

    Init

---
+++ app/Http/Controllers/ApiTestController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+require __DIR__ . '\..\..\..\vendor\autoload.php';
+
+use RapidApi\RapidApiConnect;
+use Illuminate\Http\Request;
+
+class ApiTestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        dd($request);
+        $rapid = new RapidApiConnect("default-application_5bc7d447e4b08725af2a7b23", "494450e4-a9a2-4a9d-b89b-6efe8d6b57fa");
+
+        $rapid->call('Github', 'createRepository', [
+            "accessToken" => "3e66fda71424b197d8510adc680322a89127c41c",
+            "name" => "TestRepository"
+        ]);
+
+        return view('welcome');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Hackathon Accelerator</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+
+
+    </head>
+    <body>
+    @extends('layouts.app')
+
+    @section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Register') }}</div>
+
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('test.store') }}">
+                            @csrf
+
+                            <div class="form-group row">
+                                <label for="teamname" class="col-md-4 col-form-label text-md-right">{{ __('Team Name') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="teamname" type="text" class="form-control{{ $errors->has('teamname') ? ' is-invalid' : '' }}" teamname="teamname" value="{{ old('teamname') }}" required autofocus>
+
+                                    @if ($errors->has('teamname'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('teamname') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="email1" class="col-md-4 col-form-label text-md-right">{{ __('Your Email') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="email1" type="email" class="form-control{{ $errors->has('email1') ? ' is-invalid' : '' }}" name="email1" value="{{ old('email1') }}" required>
+
+                                    @if ($errors->has('email1'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('email1') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+                            <!--2つめ-->
+                            <div class="form-group row">
+                                <label for="email2" class="col-md-4 col-form-label text-md-right">{{ __('Members Email') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="email2" type="email" class="form-control{{ $errors->has('email2') ? ' is-invalid' : '' }}" name="email2" value="{{ old('email2') }}" required>
+
+                                    @if ($errors->has('email2'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('email2') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group row mb-0">
+                                <div class="col-md-6 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('Register') }}
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @endsection
+
+    </body>
+</html>
